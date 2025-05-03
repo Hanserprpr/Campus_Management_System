06082d0ebed8d3001ad67cb50e8c8aadc17bf327
Hanserprpr <2041283903@qq.com>
2025-05-03 23:40:32 +0800

    获取自己本学期成绩

--- src/main/java/com/orbithy/cms/mapper/GradeMapper.java
+++ src/main/java/com/orbithy/cms/mapper/GradeMapper.java
@@ -1,6 +1,7 @@
 package com.orbithy.cms.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.orbithy.cms.data.dto.GradeTermDTO;
 import com.orbithy.cms.data.po.Grade;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -15,4 +16,11 @@ public interface GradeMapper extends BaseMapper<Grade> {
 
     @Update("UPDATE classes SET published = true WHERE id = #{courseId}")
     void releaseGrade(int courseId);
+
+    @Select("SELECT g.id, g.course_id, g.regular, g.final, g.grade, g.rank, c.class_num, c.point, c.teacher_id, c.type, u.username " +
+            "FROM grade g " +
+            "JOIN classes c ON g.course_id = c.id " +
+            "JOIN user u ON c.teacher_id = u.id "+
+            "WHERE student_id = #{id} AND g.term = #{term}")
+    List<GradeTermDTO> getGradeByTerm(String id, String term);
 }
