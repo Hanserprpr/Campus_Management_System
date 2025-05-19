854bb260e541bd1552045686a679b47fb80d97fe
STN2H4 <3256360589@qq.com>
2025-05-19 21:33:00 +0800

    no改为n

--- src/main/java/com/orbithy/cms/mapper/NoticeMapper.java
+++ src/main/java/com/orbithy/cms/mapper/NoticeMapper.java
@@ -10,19 +10,19 @@ import java.util.List;
 
 @Mapper
 public interface NoticeMapper extends BaseMapper<Notice> {
-    @Select("SELECT no.* , us.username AS username FROM notice no JOIN user us ON no.creator_id = us.id " +
+    @Select("SELECT n.* , us.username AS username FROM notice n JOIN user us ON n.creator_id = us.id " +
             "WHERE visible_scope = #{permission} AND status = 1 ORDER BY publish_time DESC")
     List<Notice> getNoticeList(int permission);
 
-    @Select("SELECT no.* , us.username AS username FROM notice no JOIN user us ON no.creator_id = us.id " +
+    @Select("SELECT n.* , us.username AS username FROM notice n JOIN user us ON n.creator_id = us.id " +
             "ORDER BY publish_time DESC")
     List<Notice> getNoticeListAll();
 
-    @Select("SELECT no.* , us.username AS username FROM notice no JOIN user us ON no.creator_id = us.id " +
+    @Select("SELECT n.* , us.username AS username FROM notice n JOIN user us ON n.creator_id = us.id " +
             "WHERE status = #{status} ORDER BY publish_time DESC")
     List<Notice> getNotice(int status);
 
-    @Update("UPDATE notice SET status = 0 FROM notice WHERE id = #{id}")
+    @Update("UPDATE notice SET status = 0  WHERE id = #{id}")
     void closeNotice(int id);
 
 
