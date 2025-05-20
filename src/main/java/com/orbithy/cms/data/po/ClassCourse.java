1881efff2ae000a8bfb5e8078d8f33ce99539ffb
Hanserprpr <2041283903@qq.com>
2025-05-20 19:13:37 +0800

    UPDATE

---
+++ src/main/java/com/orbithy/cms/data/po/ClassCourse.java
@@ -0,0 +1,8 @@
+package com.orbithy.cms.data.po;
+
+public class ClassCourse {
+    private Integer id;
+    private Integer classId;
+    private Integer courseId;
+    private Boolean isRequired;
+}
