9d599ee82dc3c60e4a0ed73d37d8bac1fc8c3508
STN2H4 <3256360589@qq.com>
2025-05-31 17:36:06 +0800

    fix

--- src/main/java/com/orbithy/cms/mapper/SectionMapper.java
+++ src/main/java/com/orbithy/cms/mapper/SectionMapper.java
@@ -11,8 +11,9 @@ import java.util.List;
 @Mapper
 public interface SectionMapper extends BaseMapper<Section> {
 
-    @Select("SELECT COUNT(*) FROM section " +
-            "WHERE grade = #{grade} AND name LIKE CONCAT('%', #{keyword}, '%')")
+    @Select("SELECT COUNT(*) FROM section s JOIN class_course cc ON s.id = cc.course_id " +
+            "JOIN classes cl ON cl.id = cc.class_id " +
+            "WHERE grade = #{grade} AND cl.name LIKE CONCAT('%', #{keyword}, '%')")
     Integer getSectionCount(@Param("grade") String grade,
                             @Param("keyword") String keyword);
 
@@ -20,8 +21,9 @@ public interface SectionMapper extends BaseMapper<Section> {
     @Select("select id from section where grade=#{grade}")
     List<Integer> getSectionIdList(String grade);
 
-    @Select("SELECT * FROM section " +
-            "WHERE grade = #{grade} AND name LIKE CONCAT('%', #{keyword}, '%') " +
+    @Select("SELECT * FROM section s JOIN class_course cc ON s.id = cc.course_id " +
+            "JOIN classes cl ON cl.id = cc.class_id " +
+            "s.grade = #{grade} AND cl.name LIKE CONCAT('%', #{keyword}, '%') " +
             "LIMIT #{size} OFFSET #{offset}")
     List<Section> getSectionList(@Param("grade") String grade,
                                  @Param("offset") int offset,
